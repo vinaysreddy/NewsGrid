c2c5669a1d9d46a163ae1ed8c4bf381d1a8b43c2
vinaysreddy <d.vinaysreddy@gmail.com>
2023-10-07 14:14:02 -0500

    added the gradients

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const API_KEY = "7e14f49bc28641ea931802d22e7275ba";
 const URL = "https://newsapi.org/v2/everything?q=";
 
-window.addEventListener("load", () => fetchNews("India"));
+window.addEventListener("load", () => fetchNews("honey sing"));
 async function fetchNews(query) {
     const response = await fetch(`${URL}${query}&apiKey=${API_KEY}`);
     const data = await response.json();
     console.log(data);
+    if (data.articles) console.log("hiihiihi");
     bindData(data.articles);
 }
 
@@ -18,22 +19,22 @@ function bindData(articles) {
     articles.forEach(article => {
         if (!article.urlToImage) return;
         const cardClone = newsCardTemplate.content.cloneNode(true);
-        fillDataInCard(cardClone,article);
+        fillDataInCard(cardClone, article);
         cardsContainer.appendChild(cardClone);
     });
 
 }
 
-function fillDataInCard(cardClone,article){
-    const newsImage=cardClone.querySelector('#news-img');
-    const newsTitle=cardClone.querySelector('#news-title');
-    const newsSource=cardClone.querySelector('#news-source');
-    const newsDescription=cardClone.querySelector('#news-desc');
+function fillDataInCard(cardClone, article) {
+    const newsImage = cardClone.querySelector('#news-img');
+    const newsTitle = cardClone.querySelector('#news-title');
+    const newsSource = cardClone.querySelector('#news-source');
+    const newsDescription = cardClone.querySelector('#news-desc');
 
 
-    newsImage.src=article.urlToImage;
-    newsTitle.innerHTML=article.title;
-    newsSource.innerHTML=article.publishedAt;
-    newsDescription.innerHTML=article.description;
+    newsImage.src = article.urlToImage;
+    newsTitle.innerHTML = article.title;
+    newsSource.innerHTML = article.publishedAt;
+    newsDescription.innerHTML = article.description;
 
 }
