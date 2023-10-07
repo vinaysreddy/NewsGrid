1ff6b9397ef72f1646961d07ec5d833ad713bca6
vinaysreddy <d.vinaysreddy@gmail.com>
2023-10-07 18:20:01 -0500

    completed the working site

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 const API_KEY = "7e14f49bc28641ea931802d22e7275ba";
 const URL = "https://newsapi.org/v2/everything?q=";
 
-window.addEventListener("load", () => fetchNews("honey sing"));
+const defaultHomepageSearchQuery = "news";
+
+window.addEventListener("load", () => fetchNews(defaultHomepageSearchQuery));
+
+
 async function fetchNews(query) {
     const response = await fetch(`${URL}${query}&apiKey=${API_KEY}`);
     const data = await response.json();
     console.log(data);
-    if (data.articles) console.log("hiihiihi");
+    if (data.code==="rateLimited") {
+        const errorCard = document.getElementById('error-message-card');
+        errorCard.style.display = "block";
+
+        const errorTitle=document.getElementById('error-title')
+        errorTitle.innerHTML=":( API call rate limit exceeded. You can only make a limited number of API calls within a specified time frame. Please wait and try again later."
+
+    } else {
+        const errorCard = document.getElementById('error-message-card');
+        errorCard.style.display = "none"; // Hide the error message if there are articles
+    }
     bindData(data.articles);
 }
 
@@ -22,7 +36,6 @@ function bindData(articles) {
         fillDataInCard(cardClone, article);
         cardsContainer.appendChild(cardClone);
     });
-
 }
 
 function fillDataInCard(cardClone, article) {
@@ -31,10 +44,48 @@ function fillDataInCard(cardClone, article) {
     const newsSource = cardClone.querySelector('#news-source');
     const newsDescription = cardClone.querySelector('#news-desc');
 
-
     newsImage.src = article.urlToImage;
     newsTitle.innerHTML = article.title;
-    newsSource.innerHTML = article.publishedAt;
     newsDescription.innerHTML = article.description;
+    const date = new Date(article.publishedAt).toLocaleString("en-US", { timeZone: "America/Chicago" });
+    newsSource.innerHTML = `${article.source.name} • ${date}`;
+    cardClone.firstElementChild.addEventListener('click', () => {
+        window.open(article.url, "_blank");
+    })
+}
+let currentSelectedNav = null;
 
+function onNavItemClick(id) {
+    fetchNews(id);
+    const navItem = document.getElementById(id);
+    console.log(navItem);
+    currentSelectedNav?.classList.remove('active');
+    currentSelectedNav = navItem;
+    console.log(currentSelectedNav);
+    currentSelectedNav.classList.add('active');
+}
+
+
+const searchButton = document.getElementById('search-button');
+const SearchText = document.getElementById('news-input');
+
+
+searchButton.addEventListener('click', handleSearch);
+
+SearchText.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        handleSearch();
+    }
+});
+
+function handleSearch() {
+    const query = SearchText.value;
+    if (!query) return;
+    fetchNews(query);
+    currentSelectedNav?.classList.remove('active');
+}
+
+
+function reloadPage() {
+    fetchNews(defaultHomepageSearchQuery);
 }
